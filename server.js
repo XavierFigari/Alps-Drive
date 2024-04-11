b56ff7b85fd3e03c79031863cc0c8032c9558818
Xavier <xavier.figari@le-campus-numerique.fr>
2024-04-11 09:33:07 +0200

    Started Docker nginx server to load web site, connect to API server on port 3000, added lines to allow CORS

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-
 const express = require('express')
-const server = express()
+const app = express()
 const port = 3000
 
-function start() {
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-    console.log(`Send request with curl localhost:${port}`)
-  })
 
-  server.get('/', (req, res) => {
-    console.log("Got it !")
-    res.send('My name is Bond. James Bond. ')
-  })
+function start() {
+    app.use(function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader('Access-Control-Allow-Methods', '*');
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        next();
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`)
+        console.log(`Send request with curl localhost:${port}`)
+    })
+
+    app.get('/', (req, res) => {
+        console.log("Got it ! ")
+        res.send('My name is Bond. James Bond. ')
+    })
 }
 
 exports.start = start;
